5521b34f3bce053753868bac11b20879620876d5
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2022-11-20 03:01:24 +0600

    react core concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const products = [
+    {name: 'Dell', price: '$5300', brand: 'Dell', genaration: '7th'},
+    {name: 'HP', price: '$5900', brand: 'HP', genaration: '6th'},
+    {name: 'ASUS', price: '$6300', brand: 'ASUS', genaration: '8th'},
+    {name: 'Lenovo', price: '$4300', brand: 'Lenovo', genaration: '5th'}
+  ]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        products.map(product=> <Product name={product.name} price={product.price} brand={product.brand} genaration={product.genaration}></Product>)
+      }
+      
+      
     </div>
   );
 }
 
+function Product(props){
+  return (
+    <div className='product'>
+      <h2>Name: {props.name}</h2>
+      <h3>Price: {props.price}</h3>
+      <h3>Brand: {props.brand}</h3>
+      <h3>Genaration: {props.genaration}</h3>
+    </div>
+  )
+}
+
+
 export default App;
+
